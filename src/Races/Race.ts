7f21761fb3bfc3bb1c3e1f02d41c991d2ab71329
Mayumi <cinthya.m.kuniyoshi@gmail.com>
2022-09-25 20:57:48 -0300

    feat: cria classe Race

---
+++ src/Races/Race.ts
@@ -0,0 +1,31 @@
+export default abstract class Race {
+  // constructor(
+  //   private _name: string,
+  //   private _dexterity: number,
+  // ) {}
+
+  private _name: string;
+  private _dexterity: number;
+
+  constructor(
+    name: string,
+    dexterity: number,
+  ) {
+    this._name = name;
+    this._dexterity = dexterity;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  get dexterity(): number {
+    return this._dexterity;
+  }
+
+  static createdRacesInstances(): number {
+    throw new Error('Not implemented');
+  }
+
+  abstract get maxLifePoints(): number;
+}
